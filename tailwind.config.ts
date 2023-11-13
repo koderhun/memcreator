f7629e430a6ca30d1fa99dd5bca572c9fee04cdd
Ramil <koderhanov@gmail.com>
2023-11-13 18:46:06 +0300

    tail

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,17 @@
  * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
  */
 
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {},
-  plugins: [require("flowbite/plugin")],
-};
+  plugins: [require('flowbite/plugin')],
+}
 
-export default config;
+export default config
