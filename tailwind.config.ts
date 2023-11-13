8b3e1b3de769e15f1f0c5b535a3940ec912daa1c
Ramil <koderhanov@gmail.com>
2023-11-13 14:30:31 +0300

    init nextjs project

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {},
+  plugins: [require("flowbite/plugin")],
+};
+
+export default config;
