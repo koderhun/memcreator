77cbb67b00f88c3e5e12fa16c09cd43503b0ff16
Ramil <koderhanov@gmail.com>
2023-11-13 14:53:39 +0300

    readme

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@
 let envImageUnoptimize = process.env.NODE_ENV !== 'production' ? false : true
 const nextConfig = {
   output: process.env.NODE_ENV !== 'production' ? undefined : 'export',
-  images: {
-    unoptimized: envImageUnoptimize,
-    remotePatterns: [
-      {
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
   reactStrictMode: true,
   swcMinify: true,
 }
