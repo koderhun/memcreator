32e70cc2854e09c18e43a18ff56ba4e4035f747e
Ramil <koderhanov@gmail.com>
2023-11-13 14:46:31 +0300

    pnpm

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+let envImageUnoptimize = process.env.NODE_ENV !== 'production' ? false : true
+const nextConfig = {
+  output: process.env.NODE_ENV !== 'production' ? undefined : 'export',
+  images: {
+    unoptimized: envImageUnoptimize,
+    remotePatterns: [
+      {
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 module.exports = nextConfig
