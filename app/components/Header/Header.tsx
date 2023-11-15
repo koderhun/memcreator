ac170fc7c3fbb589bf9ceb9e99d0fa29593698e9
Ramil <koderhanov@gmail.com>
2023-11-15 10:03:20 +0300

    its work

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,36 +1,26 @@
+'use client'
 import {FC} from 'react'
+import Link from 'next/link'
+import cn from 'classnames'
 import {Navbar} from 'flowbite-react'
-import Image from 'next/image'
 
 import {HeaderProps} from './Header.props'
-import s from './styles.module.scss'
 
 export const Header: FC<HeaderProps> = (props) => {
   return (
-    <div className={s.Header} {...props}>
-      <Navbar fluid={true} rounded={true}>
-        <Navbar.Brand href="https://flowbite.com/">
-          <Image
-            sizes=""
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Flowbite
-          </span>
+    <Navbar fluid {...props} className={cn(props?.className, 'bg-gray-300')}>
+      <div className="container mx-auto px-4">
+        <Navbar.Brand as={Link} href="/" title="Mem Creator">
+          <svg
+            className="w-6 h-6 text-sky-900"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 21 21">
+            <path d="M20.817 9.085a10 10 0 0 0-19.77 2.9A10.108 10.108 0 0 0 6.762 20a9.689 9.689 0 0 0 4.2 1h.012a3.011 3.011 0 0 0 2.144-.884A2.968 2.968 0 0 0 14 18v-.86A1.041 1.041 0 0 1 15 16h2.7a2.976 2.976 0 0 0 2.838-2.024 9.93 9.93 0 0 0 .279-4.891ZM5.5 12a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm2.707-3.793a1 1 0 1 1-1.414-1.414 1 1 0 0 1 1.414 1.414Zm2.872-1.624a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm4.128 1.624a1 1 0 1 1-1.414-1.413 1 1 0 0 1 1.414 1.413Z" />
+          </svg>
         </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Navbar.Link href="/navbars" active={true}>
-            Home
-          </Navbar.Link>
-          <Navbar.Link href="/navbars">About</Navbar.Link>
-          <Navbar.Link href="/navbars">Services</Navbar.Link>
-          <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-          <Navbar.Link href="/navbars">Contact</Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+      </div>
+    </Navbar>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body>
-        <main>
-          <Header />
-        </main>
+      <body suppressHydrationWarning={true}>
+        <Header />
+        <main className="container mx-auto p-4">{children}</main>
       </body>
     </html>
   )
