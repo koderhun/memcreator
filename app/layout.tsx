7d68900075fe3998a1436b3f8f24a7e0ee8601bd
Ramil <koderhanov@gmail.com>
2023-11-19 19:17:49 +0300

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,25 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="ru">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="pwa/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="pwa/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="pwa/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="apple-mobile-web-app-title" content="MemCreator" />
+        <meta name="application-name" content="MemCreator" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body suppressHydrationWarning={true}>
         <Header />
         <main className="container mx-auto p-4">{children}</main>
