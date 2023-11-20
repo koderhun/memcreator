9add36badef0167c1262314748569f2d6d8979fa
Ramil <koderhanov@gmail.com>
2023-11-20 16:41:54 +0300

    pwa2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  disable: process.env.NODE_ENV === 'development', // отключить pwa для разработки
   dest: 'public',
 })
-let envImageUnoptimize = process.env.NODE_ENV !== 'production' ? false : true
 
 module.exports = withPWA({
-  output: process.env.NODE_ENV !== 'production' ? undefined : 'export',
-  reactStrictMode: true,
-  swcMinify: true,
+  // next.js config
 })
