8e1d2fc29fe3bcb807309d61652cf8f3723feeaf
Ramil <koderhanov@gmail.com>
2023-11-20 16:46:14 +0300

    pwa3

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
+
 const withPWA = require('next-pwa')({
   dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 })
 
-module.exports = withPWA({
-  // next.js config
+let envImageUnoptimize = process.env.NODE_ENV !== 'production' ? false : true
+const nextConfig = withPWA({
+  output: process.env.NODE_ENV !== 'production' ? undefined : 'export',
+  images: {
+    unoptimized: envImageUnoptimize,
+    remotePatterns: [
+      {
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 })
+
+module.exports = nextConfig
